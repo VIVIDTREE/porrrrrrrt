5855eaf921570c027d1fe1f591db478426a8fdd7
vividtree <koomi109@naver.com>
2024-05-08 20:04:34 +0900

    sanity

--- sanity.config.js
+++ sanity.config.js
@@ -5,7 +5,6 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schema";
@@ -22,27 +21,5 @@ export default defineConfig({
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
-    dashboardTool({
-      widgets: [
-        netlifyWidget({
-          title: "Sanity Deployment",
-          sites: [
-            {
-              title: "Sanity Studio",
-              apiId: "5acaf497-bc19-49e1-8bf0-1cb4a5cb7b7a",
-              buildHookId: "663b0318717a909a87c963f7",
-              name: "sanity nstudio",
-            },
-            {
-              title: "Website",
-              apiId: "5acaf497-bc19-49e1-8bf0-1cb4a5cb7b7a",
-              buildHookId: "663b0318717a909a87c963f7",
-              name: "simwongi",
-              url: "https://my-sanity-deployment.com",
-            },
-          ],
-        }),
-      ],
-    }),
   ],
 });
--- sanity.js
+++ sanity.js
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: "xi2zimpb",
   dataset: "production",
-  apiVersion: "2024-03-19",
+  apiVersion: "2024-05-08",
   // unless you have caching for your front end, `useCdn` should be `true` for most production environments
   useCdn: true,
 });
