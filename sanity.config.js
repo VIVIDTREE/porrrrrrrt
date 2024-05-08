abfba9a532e288d9c4242e2afce4450105bf27de
vividtree <koomi109@naver.com>
2024-05-08 22:28:25 +0900

    add vercel addon

--- sanity.config.js
+++ sanity.config.js
@@ -8,6 +8,7 @@ import { structureTool } from "sanity/structure";
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schema";
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 
 export default defineConfig({
   basePath: "/sanity",
@@ -21,5 +22,6 @@ export default defineConfig({
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
+    vercelDeployTool(),
   ],
 });
