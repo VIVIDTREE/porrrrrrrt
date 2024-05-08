9907b97d5b64d7665c6b6029bd078d3ca60efc61
vividtree <koomi109@naver.com>
2024-05-08 18:45:11 +0900

    sanity

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-// ES 모듈 문법을 사용하여 Next.js 설정을 정의합니다.
 const nextConfig = {
-  output: "export", // Next.js의 새 정적 출력 옵션
   images: {
     domains: ["cdn.sanity.io"],
   },
-  reactStrictMode: true, // React의 엄격 모드 활성화 (선택적)
 };
 
 export default nextConfig;
