8414a7b7f3e3ac5eff21bf5c263e7be03fc65e1e
vividtree <koomi109@naver.com>
2024-05-10 22:36:22 +0900

    fix

--- src/app/Loading.js
+++ src/app/Loading.js
@@ -3,17 +3,20 @@ import "./loading.css"; // CSS 스타일 임포트
 
 const Loading = ({ className }) => {
   useEffect(() => {
-    // 컴포넌트가 마운트될 때 뷰포트 높이를 계산하여 적용
+    // 뷰포트 높이를 계산하고 업데이트하는 함수
     const updateHeight = () => {
       const viewportHeight = window.innerHeight + "px";
       document.querySelector(".loading-warp").style.height = viewportHeight;
     };
 
-    updateHeight(); // 초기 높이 설정
-    window.addEventListener("resize", updateHeight); // 창 크기가 변경될 때마다 높이 업데이트
+    // 초기 로드 시 및 창 크기 변경, 탭 가시성 변경 시 높이 업데이트
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    document.addEventListener("visibilitychange", updateHeight);
 
     return () => {
-      window.removeEventListener("resize", updateHeight); // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      window.removeEventListener("resize", updateHeight); // 이벤트 리스너 제거
+      document.removeEventListener("visibilitychange", updateHeight);
     };
   }, []);
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import CategoryAndSwiper from "../comp/bigcategory/CategorySwiper.js";
 import MainGroup from "../comp/main/mainGroup.js";
 import ViewAllSection from "../comp/ViewAll/viewAllSection.js";
@@ -9,23 +9,8 @@ import Footer from "../comp/footer.js";
 import "./index.css";
 
 const IndexPage = () => {
-  useEffect(() => {
-    // 뷰포트 높이를 계산하여 main-pc 클래스에 적용
-    const updateHeight = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    updateHeight(); // 초기 로드와 창 크기 변경 시 높이 업데이트
-    window.addEventListener("resize", updateHeight);
-
-    return () => {
-      window.removeEventListener("resize", updateHeight); // 이벤트 리스너 제거
-    };
-  }, []);
-
   return (
-    <main className='main-pc' style={{ height: "calc(var(--vh, 1vh) * 100)" }}>
+    <main className='main-pc'>
       <MainGroup />
       <CategoryAndSwiper />
       <RollTextBar />
@@ -37,5 +22,4 @@ const IndexPage = () => {
     </main>
   );
 };
-
 export default IndexPage;
