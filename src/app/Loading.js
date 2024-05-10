6020ae6ee92b20f845af2c7c4204fd3dd4a0ce32
vividtree <koomi109@naver.com>
2024-05-10 22:17:29 +0900

    loading page height fix

--- src/app/Loading.js
+++ src/app/Loading.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./loading.css"; // CSS 스타일 임포트
 
 const Loading = ({ className }) => {
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 뷰포트 높이를 계산하여 적용
+    const updateHeight = () => {
+      const viewportHeight = window.innerHeight + "px";
+      document.querySelector(".loading-warp").style.height = viewportHeight;
+    };
+
+    updateHeight(); // 초기 높이 설정
+    window.addEventListener("resize", updateHeight); // 창 크기가 변경될 때마다 높이 업데이트
+
+    return () => {
+      window.removeEventListener("resize", updateHeight); // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    };
+  }, []);
+
   return (
     <div className={`loading-warp ${className}`}>
       <div className='noise'></div>
