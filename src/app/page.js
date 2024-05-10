ca19e1172a978cdf47d6a98067244af5a010b164
vividtree <koomi109@naver.com>
2024-05-10 22:25:32 +0900

    hight

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import CategoryAndSwiper from "../comp/bigcategory/CategorySwiper.js";
 import MainGroup from "../comp/main/mainGroup.js";
 import ViewAllSection from "../comp/ViewAll/viewAllSection.js";
@@ -9,8 +9,23 @@ import Footer from "../comp/footer.js";
 import "./index.css";
 
 const IndexPage = () => {
+  useEffect(() => {
+    // 뷰포트 높이를 계산하여 main-pc 클래스에 적용
+    const updateHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    updateHeight(); // 초기 로드와 창 크기 변경 시 높이 업데이트
+    window.addEventListener("resize", updateHeight);
+
+    return () => {
+      window.removeEventListener("resize", updateHeight); // 이벤트 리스너 제거
+    };
+  }, []);
+
   return (
-    <main className='main-pc'>
+    <main className='main-pc' style={{ height: "calc(var(--vh, 1vh) * 100)" }}>
       <MainGroup />
       <CategoryAndSwiper />
       <RollTextBar />
@@ -22,4 +37,5 @@ const IndexPage = () => {
     </main>
   );
 };
+
 export default IndexPage;
